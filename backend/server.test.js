23c983d09fc8395226cdbef66a581d7a037ce230
NDuris <nduris@hotmail.com>
2024-06-12 17:42:49 +0200

    Testing Test

--- backend/server.test.js
+++ backend/server.test.js
@@ -10,8 +10,8 @@ describe('GET /add', () => {
   it('responds with the sum of two numbers', done => {
     // Send anmodning til /add med to tal som query parametre
     request(app)
-      .get('/add')
-      .query({ num1: 5, num2: 3 }) // Eksempel på to tal, der skal tilføjes
+      .get('/add?num1=10&num2=3')
+      //.query({ num1: 5, num2: 3 }) // Eksempel på to tal, der skal tilføjes
       .expect('Sum of 5 and 3 is 8.') // Forvent den korrekte svar tekst
       .end((err, res) => {
         if (err) return done(err);
